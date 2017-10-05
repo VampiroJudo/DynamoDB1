0e12f921ea2b36ea367684028719705b0f24e2ae
John <johnconley507@hotmail.com>
2017-10-04 21:33:33 -0400

    setting up OPI for high level demo

--- demos/demo/src/main/java/com/pluralsight/dynamodb/Main.java
+++ demos/demo/src/main/java/com/pluralsight/dynamodb/Main.java
@@ -28,8 +28,47 @@ public class Main {
 					.build();
 			
 			lowLevelDemo(client);
+			highLevelDemo(client);
 		}
 		
+		private static void highLevelDemo(AmazonDynamoDB client) {
+			ItemDao itemDao = new ItemDao(client);
+			
+			Item item1 = itemDao.put(newItem("Lawn Mower", "The very best"));
+			Item item2 = itemDao.put(newItem("Apple TV", "Black and White"));
+			Item item3 = itemDao.put(newItem("Apple laptop", "With Windows XP"));
+			
+			itemDao.delete(item2.getId());
+			print(itemDao.getAll());
+			
+		}
+		
+		
+		private static void print(List<Item> all) {
+			System.out.println(all.stream()
+					.map(Item::toString)
+					.collect(Collectors.joining( delimiter: "\n")));
+		}
+		
+		private static Item newItem(String name, String description) {
+			Item item = new Item();
+			item.setName(name);
+			item.setDescription(description);
+			
+			return item;
+		}
+		
+
+		private static void pause() {
+			System.out.println("PAUSE");
+			try {
+				System.in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+
 		private static void lowLevelDemo(AmazonDynamoDB client) {
 			ItemDao itemDao = new ItemDao(client);
 			
@@ -39,5 +78,7 @@ public class Main {
 			
 			itemDao.put(item);
 		}
-    
 }
+		
+		
+    
