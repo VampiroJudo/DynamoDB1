c0582acbfe33bde515efa8bc5c5690cf94e46b35
John <johnconley507@hotmail.com>
2017-10-05 17:44:54 -0400

    adding get,g put, get, delete and getAll methods to comments

--- demos/demo/src/main/java/com/pluralsight/dynamodb/dao/CommentDao.java
+++ demos/demo/src/main/java/com/pluralsight/dynamodb/dao/CommentDao.java
@@ -14,5 +14,35 @@ import com.pluralsight.dynamodb.domain.Item;
 
 import java.util.List;
 
-
+public class CommentDao {
+	private DynamoDBMapper mapper;
+	
+	public CommentDao(AmazonDynamoDB dynamoDB) {
+		mapper = new DynamoDBMapper(dynamoDB);
+	}
+	
+	public Comment put(Comment comment) {
+		mapper.save(comment);
+		return comment;
+	}
+	
+	public Comment get(String itemId, String messageId) {
+		Comment comment = new Comment();
+		comment.setItemId(itemId);
+		comment.setMessage(messageId);
+		
+		return mapper.load(comment);
+	}
+	
+	public void delete(String itenId, String messageId) {
+		Comment comment = new Comment();
+		comment.setItemId(itemId);
+		comment.setMessageId(messageId);
+		
+		mapper.delete(comment);
+	}
+	
+	public List<Comment> getAll() {
+		return mapper.scan(Comment.class, new DynamoDBScanExpression());
+	}
 }
